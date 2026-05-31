52ab26a1e2cbb2688fd36089e3261be7d8ec9779
lionel-rowe <lionel.rowe@gmail.com>
2026-05-31 15:31:37 +0100

    Build JS on-the-fly on first request

--- scripts/devServer.ts
+++ scripts/devServer.ts
@@ -1,18 +1,43 @@
 import { join } from '@std/path'
 import { serveDir } from '@std/http/file-server'
+import { exists } from '@std/fs'
 
-await new Deno.Command(Deno.execPath(), {
-	args: [
-		'bundle',
-		join('src', 'web', 'index.ts'),
-		['-o', join('web', 'index.mjs')],
-		['--format', 'esm'],
-	].flat(),
-}).spawn().output()
+const IS_DEV_MODE = !Deno.env.get('DENO_DEPLOY')
+console.info(`Running in ${IS_DEV_MODE ? 'DEV' : 'PROD'} mode`)
+
+const pathMap: Partial<Record<string, (outfile: string) => Promise<void> | void>> = {
+	async '/index.mjs'(outFile: string) {
+		const { denoPlugin } = await import('@deno/esbuild-plugin')
+		const { build } = await import('esbuild')
+
+		await build({
+			entryPoints: [
+				join('src', 'web', 'index.ts'),
+			],
+			bundle: true,
+			outfile: outFile,
+			platform: 'browser',
+			format: 'esm',
+			plugins: [denoPlugin()],
+		})
+	},
+}
 
 Deno.serve({
 	port: 9999,
-	handler(req) {
+	async handler(req) {
+		const url = new URL(req.url)
+		const path = url.pathname
+
+		if (Object.hasOwn(pathMap, path)) {
+			const buildFn = pathMap[path]!
+			const outPath = join('web', `.${path}`)
+			if (IS_DEV_MODE || !(await exists(outPath))) {
+				console.info(`Building ${path}...`)
+				await buildFn(outPath)
+			}
+		}
+
 		return serveDir(req, {
 			fsRoot: 'web',
 			urlRoot: '',
