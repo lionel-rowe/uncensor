0ed1f8f8ab3371c80831da349105f98b5ad37efd
lionel-rowe <lionel.rowe@gmail.com>
2026-06-01 13:06:41 +0100

    Remove links from markdown headings

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,30 @@
 import { join } from '@std/path'
 import { serveDir } from '@std/http/file-server'
 import { exists } from '@std/fs'
-import { CSS, render } from '@deno/gfm'
 import { STATUS_CODE } from '@std/http'
+import markedAlert from 'marked-alert'
+import { Marked } from 'marked'
+import { CSS } from '@deno/gfm'
 
 const IS_DEV_MODE = !Deno.env.get('DENO_DEPLOY')
 console.info(`Running in ${IS_DEV_MODE ? 'DEV' : 'PROD'} mode`)
 
-const pathMap: Partial<Record<string, (outfile: string) => Promise<void> | void>> = {
+const buildFnsByPath: Partial<Record<string, (outfile: string) => Promise<void> | void>> = {
 	async '/'() {
 		const template = await Deno.readTextFile(join('src', 'web', 'index.html'))
+
+		const marked = new Marked()
+		marked.use(markedAlert())
 
 		const instructions = `
 			<link rel="stylesheet" href="gfm.css">
 			<div data-color-mode="light" data-light-theme="light" data-dark-theme="dark" class="markdown-body">
-				${render(await Deno.readTextFile(join('src', 'web', 'instructions.md')))}
+				${marked.parse(await Deno.readTextFile(join('src', 'web', 'instructions.md')))}
 			</div>
 		`
 
 		const html = template.replace(/<!--\s*INSTRUCTIONS\s*-->/, instructions)
-
-		await Deno.writeTextFile(
-			join('web', 'index.html'),
-			html,
-		)
+		await Deno.writeTextFile(join('web', 'index.html'), html)
 	},
 	async '/gfm.css'() {
 		await Deno.writeTextFile(join('web', 'gfm.css'), CSS)
@@ -53,8 +54,8 @@ Deno.serve({
 		const url = new URL(req.url)
 		const path = url.pathname
 
-		if (Object.hasOwn(pathMap, path)) {
-			const buildFn = pathMap[path]!
+		if (Object.hasOwn(buildFnsByPath, path)) {
+			const buildFn = buildFnsByPath[path]!
 			const outPath = join('web', `.${path === '/' ? '/index.html' : path}`)
 			if (IS_DEV_MODE || !(await exists(outPath))) {
 				console.info(`Building ${path}...`)
